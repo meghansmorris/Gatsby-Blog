e9f8c30cf6668baa2d96719f694aa32724251d7f
itsmegh <42684917+itsmegh@users.noreply.github.com>
2019-05-23 11:34:43 -0600

    setting up files

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-catch-links`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,6 +14,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,33 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
+const Header = () => (
+  <div
     style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
+      background: '#f5f5f5',
+      marginBottom: '3rem',
+      borderBottom: '2px solid #e6e6e6',
     }}
   >
     <div
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        margin: '0 auto',
+        maxWidth: 980,
+        padding: '1.45rem 1.0875rem',
       }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+   >
+     <h1 style={{margin: 0, textAlign: 'center',fontSize: '18px'}}>
+        <Link to="/"
           style={{
-            color: `white`,
-            textDecoration: `none`,
+            color: 'black',
+            textDecoration: 'none',
           }}
         >
-          {siteTitle}
+          CodeStack
         </Link>
       </h1>
     </div>
-  </header>
-)
+  </div>
+);
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
+export default Header;
